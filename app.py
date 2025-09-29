f8c78212ea99c3a5d839fb7aabef882b478cf401
chuqudee-sand <cokereafor@sandtech.com>
2025-09-29 11:51:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ if not AWS_S3_BUCKET:
     raise Exception("AWS_S3_BUCKET environment variable not set")
 
 s3 = boto3.client('s3')
-CSV_OBJECT_KEY = 'ai_peer-matcing_data.csv'
+CSV_OBJECT_KEY = 'aice_peer-matcing_data.csv'
 
 ADMIN_PASSWORD = "alx_admin_2025_peer_finder"
 
@@ -603,3 +603,4 @@ def disclaimer():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
